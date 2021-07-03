e0859c73a3d476ca8017e680aa5b8111ccab5d1f
Meshari Aldoukhi <mashry151@hotmail.com>
2021-07-03 20:32:14 +0300

    Solved

--- data.js
+++ data.js
@@ -2,21 +2,25 @@ const data = [
     {
         id: 1,
         name: "toDo 1",
+        priority: "HIGH",
         stat: false,
     },
     {
         id: 2,
         name: "toDo 2",
+        priority: "HIGH",
         stat: false,
     },
     {
         id: 3,
         name: "toDo 3",
+        priority: "HIGH",
         stat: false,
     },
     {
         id: 4,
         name: "toDo 4",
+        priority: "HIGH",
         stat: false,
     },
 ];
